a75af822d5819900f28b3671d561a70a307fa0fe
Shooter Black Rock <jiaxinyang74@126.com>
2018-05-19 14:28:37 +0800

    mas

---
+++ Mp3Window/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Mp3Window
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Button CloseButton;
+        private Button MinButton;
+        private TextBlock WindowTitleTbl;
+
+        public MainWindow()
+        {
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            ControlTemplate MainWindowTemplate
+                = Application.Current.Resources["MainWindowTemplate"] as ControlTemplate;
+
+            if (MainWindowTemplate!=null)
+            {
+                CloseButton = MainWindowTemplate.FindName("CloseWinButton", this) as Button;
+                MinButton = MainWindowTemplate.FindName("MinWinButton", this) as Button;
+
+                CloseButton.Click += CloseButton_Click;
+                MinButton.Click += MinButton_Click;
+
+                WindowTitleTbl = MainWindowTemplate.FindName("WindowTitleTbl", this) as TextBlock;
+            }
+
+          }
+        private void CloseButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MinButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.WindowState = System.Windows.WindowState.Minimized;
+        }
+
+        /// <summary>
+        /// 实现窗体移动
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            DragMove();
+
+            base.OnMouseLeftButtonDown(e);
+        }
+
+    }
+
+
+
+}
